af61c55dc1e2852f6c2215c11ef65177df1701c3
Bree2003 <breeale2003@gmail.com>
2025-09-05 20:48:00 -0400

    subida clase 3

---
+++ Clase-3/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# Versión simple
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hola, Flask! Esta es la versión simple"
+
+# Renderizar HTMLS
+# def create_app():
+    # app = Flask(__name__)
+    
+    # Importar rutas
+    # from . import routes
+    # app.register_blueprint(routes.bp)
+    
+    # return app
---
+++ Clase-3/run.py
@@ -0,0 +1,10 @@
+# Renderizar htmls
+# from app import create_app
+
+# app = create_app()
+
+# Versión simple
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
